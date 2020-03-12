023151a3c5edbd0e4f38d4318443ba740a44592e
manato.kuroda <a7108100@gmail.com>
2020-03-12 16:52:25 +0900

    refactor: make detect url generic

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,6 +1,8 @@
+import { isMediumEditURL, isMediumURL } from 'src/utils/isMediumURL'
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.url && /https?:\/\/medium\.com/.test(changeInfo.url)) {
-    const active = /https?:\/\/medium\.com\/p\/.*\/edit/.test(changeInfo.url)
+  if (changeInfo.url && isMediumURL(changeInfo.url)) {
+    const active = isMediumEditURL(changeInfo.url)
 
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
       chrome.tabs.sendMessage(tabs[0].id as number, { active })
---
+++ src/utils/isMediumURL.ts
@@ -0,0 +1,3 @@
+export const isMediumURL = (url: string) => /https?:\/\/medium\.com/.test(url)
+export const isMediumEditURL = (url: string) =>
+  /https?:\/\/medium\.com\/p\/.*\/edit/.test(url)
